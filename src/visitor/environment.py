24bc99edb30e3bdf62de36e9f402f5d8fc081d7d
Malgorzata_Kozlowska <01169195@pw.edu.pl>
2024-06-05 17:16:28 +0200

    feat: initial implementation of embedded classes

--- src/visitor/environment.py
+++ src/visitor/environment.py
@@ -19,8 +19,64 @@ class Value:
         return self.value
 
 
+class FunctionValue:
+    def __init__(self,
+                 value: "Args",
+                 type: AstType = AstType.TYPE_FUNCTION,
+                 return_value: AstType = AstType.NULL) -> None:
+
+        self.value = value
+        self.type = type
+        self.call_count = 1
+        self.return_value = return_value
+
+    @property
+    def args(self) -> "Args":
+        return self.value
+
+    @property
+    def return_value(self) -> AstType:
+        return self._return_value
+
+
+class Args:
+
+    def __init__(self,
+                 value: list['Param']) -> None:
+
+        self.value = value
+
+    @property
+    def count(self) -> int:
+        return len(self.value)
+
+
+class Param:
+    def __init__(self,
+                 name: str,
+                 value: Any,
+                 type: AstType) -> None:
+
+        self.name = name
+        self.value = value
+        self.type = type
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def value(self) -> Any:
+        return self._value
+
+    @property
+    def type(self) -> AstType:
+        return self._type
+
+
 class Scope:
     def __init__(self) -> None:
+
         self.variables = {}
 
     def find_and_set_old_variable(self, name: str,  value: Value):  # todo, aby sprawdzać czy zmienny różnych typów o tej samej nazwie
@@ -30,50 +86,66 @@ class Scope:
                 return True
             else:
                 existing_variable_type = self.variables[name].type
-            raise VariableNameConflictError(name, value.type, existing_variable_type)
+            raise VariableNameConflictError(name,
+                                            value.type,
+                                            existing_variable_type)
         return False
 
-    def add_variable(self, name: str, value: Value) -> None:
+    def add_variable(self,
+                     name: str,
+                     value: Value) -> None:
         if not self.find_and_set_old_variable(name, value):
             self.variables[name] = value
 
-    def get_variable(self, variable_name: str) -> Value:
+    def get_variable(self,
+                     variable_name: str) -> Value:
         if (value := self.variables.get(variable_name)):
             return value
         return None
 
 
 class CallContext:
-    def __init__(self, function_name: str, expected_return_type: Any) -> None:
+    def __init__(self,
+                 function_name: str,
+                 expected_return_type: Any) -> None:
+
         self.function_name = function_name
         self.expected_return_type = expected_return_type
         self.scopes = [Scope()]
 
-    def get_variable(self, variable_name: str) -> Value | None:
+    def get_variable(self,
+                     variable_name: str) -> Value | None:
         for scope in reversed(self.scopes):
             if (value := scope.get_variable(variable_name)) is not None:
                 return value
         return None
 
-    def add_variable(self, name: str, value: Value) -> None:
+    def add_variable(self,
+                     name: str,
+                     value: Value) -> None:
         # check_if_variable_exists
         self.scopes[-1].add_variable(name, value)
 
     def add_scope(self) -> None:
+
         self.scopes.append(Scope())
 
     def delete_scope(self) -> None:
+
         self.scopes.pop()
 
 
 class Environment:
     def __init__(self) -> None:
+
         self.global_scope = Scope()
         self.current_scope = self.global_scope
-        self.call_contexts = []  # [CallContext(Scope, Scope...), CallContext, ...]
+        self.call_contexts = []
         self.nesting_level = 0
 
-    def enter_function_call(self, function_name: str, return_type: Any) -> None:
+    def enter_function_call(self,
+                            function_name: str,
+                            return_type: Any) -> None:
         self.nesting_level += 1
         self.call_contexts.append(CallContext(function_name, return_type))
         self.current_scope = self.call_contexts[-1].scopes[-1]
@@ -93,12 +165,13 @@ class Environment:
         self.call_contexts[-1].delete_scope()
 
     def get_variable(self, variable_name: str) -> Value:
-        return self.current_scope.get_variable(variable_name)
+        return self.current_scope.get_variable(variable_name) or self.global_scope.get_variable(variable_name)
 
     def add_variable(self, name: str, value: Value) -> None:
         self.current_scope.add_variable(name, value)
 
-
+    def add_global_variable(self, name: str, value: Value) -> None:
+        self.global_scope.add_variable(name, value)
     # def create_parameters_scope(self, input_parameters: list, provided_arguments: list) -> None:
     #     initial_scope = Scope(self.current_scope)
     #     # błąd jeśli nie jest taka sama liczba parametrow i argumentów
